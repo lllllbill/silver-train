1708c2bc8d4704af158ba7800142409484c9ffe7
Bill <592383484@qq.com>
2020-06-22 22:51:43 +0800

    搞错了，再来

--- silver-train-scheduler/src/main/java/org/silver/train/excutor/AbstractExcutor.java
+++
@@ -1,9 +0,0 @@
-package org.silver.train.excutor;
-
-import org.silver.train.task.TemporalFrequency;
-
-public abstract class AbstractExcutor implements Excutor{
-	
-	public abstract String changeTemporalFrequency(TemporalFrequency temporalFrequency);
-	
-}
--- silver-train-scheduler/src/main/java/org/silver/train/scheduler/LinkedTask.java
+++
@@ -1,8 +0,0 @@
-package org.silver.train.scheduler;
-
-import org.silver.train.task.Task;
-
-public class LinkedTask {
-	LinkedTask next;
-	Task task;
-}
