80f5300a64e220619b744bdf3dacaab9c9bccb0e
Bill <592383484@qq.com>
2020-06-26 23:57:46 +0800

    quartZ init

---
+++ silver-train-schedule/src/main/java/quartz/InitQuartzJob.java
@@ -0,0 +1,13 @@
+package quartz;
+/**
+ * 
+* @Title: InitQuartzJob.java
+* @Package quartz
+* @Description: 初始化定时任务
+* @author Bill
+* @date 2020年6月26日
+* @version V1.0
+ */
+public class InitQuartzJob {
+
+}
