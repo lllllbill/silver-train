0061f3a9f2e343338f6035db40c1eae2f9119dd1
Bill <592383484@qq.com>
2020-08-15 23:14:36 +0800

    LinkedPageProcessor commit

--- silver-train-spider/src/main/java/com/silverTrain/spider/pageProcessor/LinkedPageProcessor.java
+++ silver-train-spider/src/main/java/com/silverTrain/spider/pageProcessor/LinkedPageProcessor.java
@@ -1,9 +1,22 @@
 package com.silverTrain.spider.pageProcessor;
 
+import java.util.List;
+
 public abstract class LinkedPageProcessor{
+	protected boolean processorStatus =false;
 	
 	protected LinkedPageProcessor parentPageProcessor;
 	
 	protected LinkedPageProcessor childPageProcessor;
+	
+	protected List<String> taskList;
+	/**
+	 * 由该processor加载到队列的url数量，当清空时表示该任务爬取结束
+	 */
+	protected int urlNum;
+	
+	public boolean getStatus(){
+		return this.processorStatus;
+	}
 	
 }
--- silver-train-spider/src/main/java/com/silverTrain/spider/pageProcessorExample/DoNewsIdonewsPageProcessor.java
+++ silver-train-spider/src/main/java/com/silverTrain/spider/pageProcessorExample/DoNewsIdonewsPageProcessor.java
@@ -1,5 +1,23 @@
 package com.silverTrain.spider.pageProcessorExample;
 
-public class DoNewsIdonewsPageProcessor {
+import com.silverTrain.spider.pageProcessor.LinkedPageProcessor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class DoNewsIdonewsPageProcessor extends LinkedPageProcessor implements PageProcessor{
+
+	@Override
+	public void process(Page page) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Site getSite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
